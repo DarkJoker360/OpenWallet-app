d6258e49eb175956d72a0a611e7f3a4d23a112a6
DarkJoker360 <simoespo159@gmail.com>
2026-02-16 15:09:47 -0800

    1.2.0 (3)
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.esposito.openwallet"
         minSdk = 31
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
